231d2620eecf2dd214e7cf26224aefd05df8dbc5
1987plus <66287510+1987plus@users.noreply.github.com>
2021-01-22 13:18:54 +0800

    Update jd_farm.py

--- jd_farm.py
+++ jd_farm.py
@@ -11,7 +11,7 @@ import notification
 2、waterTimesLimit 自定义的每天浇水最大次数
 3、retainWaterLimit 完成10次浇水任务的基础上,希望水滴始终高于此数;优先级高于waterTimesLimit
 4、水滴高于100时,默认使用翻倍卡;其他情况不使用道具
-5、cron 35 6,11,17,23 * * *
+5、cron 35 5,10,16,22 * * *
 """
 waterTimesLimit = 20  # 自定义的每天浇水最大次数
 retainWaterLimit = 100  # 完成10次浇水任务的基础上,希望水滴始终高于此数
